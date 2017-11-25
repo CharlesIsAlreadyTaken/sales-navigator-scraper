fd8e3170569956e632723ae27ebe2da3897d2714
CharlesIsAlreadyTaken <charlesalbaut@gmail.com>
2017-11-25 18:57:02 -0500

    Logs + var handling

--- js/parser.js
+++ js/parser.js
@@ -1,4 +1,8 @@
-let count = $('span.spotlight-result-count:first').text() | 0;
+if (!count) {
+    let count = 0;
+}
+count = $('span.spotlight-result-count:first').text();
+console.log('Count is: ', count);
 if (count.indexOf('K') >= 0 || count.indexOf('.') >= 0) {
     count = 1000;
 }
